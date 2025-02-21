47683310be892f12a83b135e1b28eb5a7e902454
Nils Friedrich <nils-jonathan.friedrich@student.tu-freiberg.de>
2025-02-21 13:16:55 +0100

    initial commit

---
+++ include/wpf_plugins/behavior_tree/analyze_logs_action.hpp
@@ -0,0 +1,57 @@
+#ifndef WPF_PLUGINS__BEHAVIOR__ACTION__ANALYZE_LOGS_ACTION_HPP_
+#define WPF_PLUGINS__BEHAVIOR__ACTION__ANALYZE_LOGS_ACTION_HPP_
+
+#include <string>
+
+#include "nav2_behavior_tree/bt_action_node.hpp"
+#include "wpf_msgs/action/analyze_logs.hpp"
+
+namespace wpf_plugins
+{
+
+/**
+ * @brief A nav2_behavior_tree::BtActionNode class that wraps wpf_msgs::action::AnalyzeLogs
+ */
+class AnalyzeLogsAction : public nav2_behavior_tree::BtActionNode<wpf_msgs::action::AnalyzeLogs>
+{
+public:
+  /**
+   * @brief A constructor for wpf_plugins::AnalyzeLogsAction
+   * @param xml_tag_name Name for the XML tag for this node
+   * @param action_name Action name this node creates a client for
+   * @param conf BT node configuration
+   */
+  AnalyzeLogsAction(
+    const std::string & xml_tag_name,
+    const std::string & action_name,
+    const BT::NodeConfiguration & conf);
+
+  /**
+   * @brief Function to perform some user-defined operation on tick
+   */
+  void on_tick() override;
+
+  /**
+   * @brief Function to read parameters and initialize class variables
+   */
+  void initialize();
+
+  /**
+   * @brief Creates list of BT ports
+   * @return BT::PortsList Containing basic ports along with node-specific ports
+   */
+  static BT::PortsList providedPorts()
+  {
+    return providedBasicPorts(
+      {
+        BT::InputPort<std::string>("logs_directory", ""),
+        BT::InputPort<bool>("overwrite_results", false, "Overwrite logs or not"),
+        BT::InputPort<float>("start_time", 0.0f, "Start timestamp for the analysis interval"),
+        BT::InputPort<float>("end_time", 0.0f, "End timestamp for the analysis interval")
+      });
+  }
+};
+
+}  // namespace wpf_plugins
+
+#endif  // WPF_PLUGINS__BEHAVIOR__ACTION__ANALYZE_LOGS_ACTION_HPP_
---
+++ src/analyze_logs_action.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <memory>
+#include <cmath>
+
+#include "wpf_plugins/behavior_tree/analyze_logs_action.hpp"
+
+namespace wpf_plugins
+{
+
+AnalyzeLogsAction::AnalyzeLogsAction(
+  const std::string & xml_tag_name,
+  const std::string & action_name,
+  const BT::NodeConfiguration & conf)
+: BtActionNode<wpf_msgs::action::AnalyzeLogs>(xml_tag_name, action_name, conf)
+{
+}
+
+void AnalyzeLogsAction::initialize()
+{
+  std::string logs_directory;
+  getInput("logs_directory", logs_directory);
+  goal_.logs_directory = logs_directory;
+  bool overwrite_results;
+  getInput("overwrite_results", overwrite_results);
+  goal_.overwrite_results = overwrite_results;
+  double start_time;
+  getInput("start_time", start_time);
+  goal_.start_time = static_cast<float>(start_time);
+  double end_time;
+  getInput("end_time", end_time);
+  goal_.end_time   = static_cast<float>(end_time);
+}
+
+void AnalyzeLogsAction::on_tick()
+{
+  if (!BT::isStatusActive(status())) {
+    initialize();
+  }
+
+  increment_recovery_count();
+}
+
+}  // namespace wpf_plugins
+
+#include "behaviortree_cpp/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  BT::NodeBuilder builder =
+    [](const std::string & name, const BT::NodeConfiguration & config)
+    {
+      return std::make_unique<wpf_plugins::AnalyzeLogsAction>(name, "analyze_logs", config);
+    };
+
+  factory.registerBuilder<wpf_plugins::AnalyzeLogsAction>("AnalyzeLogs", builder);
+}
